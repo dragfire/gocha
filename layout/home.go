698550adbdbe7d301f88f9048018552801f41975
dragfire <asem.devajit@gmail.com>
2016-08-19 01:01:36 +0530

    added seperate keyhandlers package

--- layout/home.go
+++ layout/home.go
@@ -1,46 +1,18 @@
 package layout
 
 import (
-	"fmt"
-	_ "log"
-
 	"github.com/jroimartin/gocui"
 )
 
-func getCmd(g *gocui.Gui, v *gocui.View) error {
-	var (
-		cmd string
-	)
-
-	cmd = v.ViewBuffer()
-
-	maxX, maxY := g.Size()
-
-	if v, err := g.SetView("msg", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		fmt.Fprintf(v, "%s", cmd)
-		if err := g.SetCurrentView("msg"); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func keybindings(g *gocui.Gui) error {
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, Quit); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("cmdbar", gocui.KeyEnter, gocui.ModNone, getCmd); err != nil {
+	if err := g.SetKeybinding("cmdbar", gocui.KeyEnter, gocui.ModNone, GetCmd); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("msg", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := g.SetKeybinding("msg", gocui.KeyCtrlC, gocui.ModNone, Quit); err != nil {
 		return err
 	}
 	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
 }
---
+++ layout/keyhandlers.go
@@ -0,0 +1,31 @@
+package layout
+
+import (
+	"fmt"
+	"github.com/jroimartin/gocui"
+)
+
+func GetCmd(g *gocui.Gui, v *gocui.View) error {
+	var (
+		cmd string
+	)
+
+	cmd = v.ViewBuffer()
+
+	maxX, maxY := g.Size()
+
+	if v, err := g.SetView("msg", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintf(v, "%s", cmd)
+		if err := g.SetCurrentView("msg"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}

806c563d8192449b6b4fc89f19643f6e42568698
dragfire <asem.devajit@gmail.com>
2016-08-21 15:23:19 +0530

    dynmic PORT switch

--- app.go
+++ app.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	_ "fmt"
-	"net/http"
-	
 	"github.com/dragfire/gocha/logger"
 	middleware "github.com/dragfire/gocha/middlewares"
 	"github.com/dragfire/gocha/server"
 	"golang.org/x/net/websocket"
+	"net/http"
+	"os"
 )
 
 func main() {
+	PORT := os.Getenv("PORT")
+
 	http.Handle("/", middleware.LogRequest(http.HandlerFunc(server.Handler)))
 	http.Handle("/view/", middleware.LogRequest(server.MakeHandler(server.ViewHandler)))
 	http.Handle("/edit/", middleware.LogRequest(server.MakeHandler(server.EditHandler)))
 	http.Handle("/save/", middleware.LogRequest(server.MakeHandler(server.SaveHandler)))
 	http.Handle("/chat", middleware.LogRequest(http.HandlerFunc(server.ClientHandler)))
 	http.Handle("/socket", websocket.Handler(server.SocketHandler))
-	logger.Info.Println("Server Listening: 8080")
+
+	if PORT == "" {
+		PORT = "3000"
+	}
+	logger.Info.Println("Server Listening:", PORT)
+
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
-	logger.Debug.Println(http.ListenAndServe(":8080", nil))
+	logger.Debug.Println(http.ListenAndServe(":"+PORT, nil))
 }

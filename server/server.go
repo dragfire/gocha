66c5099c10bcfbcad7a5f5281586eec6c46cf71a
dragfire <asem.devajit@gmail.com>
2016-10-21 21:42:56 +0530

    Export Main

--- server/server.go
+++ server/server.go
@@ -8,6 +8,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"os"
+	"github.com/dragfire/gocha/middlewares"
+	"golang.org/x/net/websocket"
+	"github.com/dragfire/gocha/socket"
 )
 
 var templates = template.Must(template.ParseFiles("views/edit.html", "views/save.html", "views/view.html"))
@@ -117,4 +121,24 @@ func MakeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 		}
 		fn(w, r, m[2])
 	}
+}
+
+func Main() {
+	PORT := os.Getenv("PORT")
+
+	http.Handle("/", middleware.LogRequest(http.HandlerFunc(Handler)))
+	http.Handle("/view/", middleware.LogRequest(MakeHandler(ViewHandler)))
+	http.Handle("/edit/", middleware.LogRequest(MakeHandler(EditHandler)))
+	http.Handle("/save/", middleware.LogRequest(MakeHandler(SaveHandler)))
+	http.Handle("/chat", middleware.LogRequest(http.HandlerFunc(ClientHandler)))
+	http.Handle("/socket", websocket.Handler(socket.Handler))
+
+	if PORT == "" {
+		PORT = "3000"
+	}
+	logger.Info.Println("Server Listening:", PORT)
+
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+	logger.Debug.Println(http.ListenAndServe(":"+PORT, nil))
+	//client.Init()
 }
